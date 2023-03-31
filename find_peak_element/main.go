89d2913a88532b90df062d4b0c24aa392e682318
Bekzod <mirzaahmedov.dev@proton.me>
2023-03-31 15:16:27 +0500

    improve find peak element

--- find_peak_element/main.go
+++ find_peak_element/main.go
@@ -40,23 +40,19 @@ func findPeakElement(nums []int) int {
   left := 0
   right := len(nums) - 1
 
-  for right - left > 2 {
+  for right > left {
     mid := (left + right) / 2
 
-    if nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] {
+    if mid > 0 && mid < len(nums) - 1 && nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] {
       return mid
     }
 
-    if nums[mid - 1] < nums[mid] {
-      right = mid - 1
-    } else {
+    if nums[mid] < nums[mid + 1] {
       left = mid + 1
+    } else {
+      right = mid - 1
     }
   }
 
-  if nums[left] > nums[right] {
-    return left
-  }
-
-  return right
+  return left
 }
